dd2ae625fc254cd656a85bf882e47d28cbc6bd7c
bosco8622 <148661875+bosco8622@users.noreply.github.com>
2025-04-26 18:26:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, my little princess👸...`;
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}, 我的小公主👸...`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
